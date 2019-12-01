7924e1c61fc49ce070a953a72cb012f6dcc31f46
Olga <abajur55@mail.ru>
2019-12-01 23:56:42 +0600

    old test dsnt work

---
+++ src/test/java/DeliveryCardTest.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+
+    import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import java.time.LocalDate;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+    class DeliveryCardTest {
+        @Test
+        void shouldSubmitRequest(){
+            open("http://localhost:9999");
+            $("[placeholder='Город']").setValue("Новосибирск");
+            LocalDate date = LocalDate.now();
+            date = date.plusDays(10);
+            $("[placeholder = 'Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            $("[placeholder = 'Дата встречи']").sendKeys(Keys.chord(Keys.DELETE));
+            int day = date.getDayOfMonth();
+            $("[placeholder = 'Дата встречи']").setValue(String.valueOf(day));
+            int month = date.getMonthValue();
+            $("[placeholder = 'Дата встречи']").setValue(String.valueOf(month));
+            int year = date.getYear();
+            $("[placeholder = 'Дата встречи']").setValue(String.valueOf(year));
+            $("button").submit();
+            $("[name = 'name']").setValue("Герман Греф");
+            $("[name = 'phone']").setValue("+12345678911");
+            $("[class='checkbox__box']").click();
+            $$("button").find(exactText("Забронировать")).click();
+            $(withText("Встреча")).waitUntil(visible, 15000);
+        }
+    }
