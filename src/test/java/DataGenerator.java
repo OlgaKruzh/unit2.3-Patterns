e1933cd5bfaa3bacce13e7a1e2575436c66cb2c8
Olga <abajur55@mail.ru>
2019-12-02 23:33:35 +0600

    long read is ready

--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -14,24 +16,25 @@ import static com.codeborne.selenide.Condition.*;
 public class DataGenerator {
     public static void main(String[] args) {
 
- //    class DataGenerator() {
+       // class DataGenerator () {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
             Faker faker = new Faker(new Locale("ru"));
             String city = faker.address().cityName();
             String name = faker.name().fullName();
             String phone = faker.phoneNumber().phoneNumber();
             LocalDate dateFirst = LocalDate.now();
             dateFirst = dateFirst.plusDays(4);
+            String dateFirstInput = dateFirst.format(formatter);
             LocalDate dateLast = dateFirst.plusDays(7);
+            String dateLastInput = dateLast.format(formatter);
 
-
-            System.out.println(city);
-            System.out.println(name);
-            System.out.println(phone);
-            System.out.println(dateFirst);
-            System.out.println(dateLast);
+            // System.out.println(name);
+            // System.out.println(dateFirstInput);
+            // System.out.println(dateLastInput);
         }
 
     }
+
 
 
 
